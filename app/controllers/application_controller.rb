63982368a2a46fe4754042cfee0e8b8b4c2afb8e
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-23 22:24:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,12 +35,13 @@ class ApplicationController < Sinatra::Base
 
   get '/posts/:id/edit' do
     @post = Post.find_by(id: params[:id])
-    
+
     erb :edit
   end
 
   patch '/posts/:id' do
     @post = Post.find_by(id: params[:id])
+    @post.update
   end
 
 end
